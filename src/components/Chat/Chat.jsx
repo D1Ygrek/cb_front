5f443938be7bdb20c6beaf6ff6de4be9cdd4d7e6
D1Ygrek <49139544+D1Ygrek@users.noreply.github.com>
2022-06-03 16:05:23 +0500

    finished workFlow

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,12 +1,51 @@
 import React, {useState, useEffect} from 'react';
-
+import { message } from 'antd';
 import { BotMessage, UserMessage } from '../Messages/Messages';
 import UserInput from '../UserInput/UserInput';
 
 import './Chat.css'
 
+async function sendChosenUpdate(
+  variant,
+  variantMessage, 
+  userId,
+  setVariants,
+  setMessages, 
+  chatMain, 
+  messages){
+  setMessages([...messages, {sender: 'user', message: variantMessage}])
+  setTimeout(() => {chatMain.scrollTop = chatMain.scrollHeight}, 40)
+  let result = await fetch(
+    'http://localhost:5000/user_variant',
+    {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({
+        id: userId,
+        variant: variant,
+        message: variantMessage
+      })
+    }
+  )
+  let status = result.status
+  if(status == 200){
+    let data = await result.json()
+    console.log(data)
+    setMessages(data.messages)
+    setVariants(data.variants)
+  }
+  else{
+    message.error('Ошибка загрузки данных')
+  }
+  setTimeout(() => {chatMain.scrollTop = chatMain.scrollHeight}, 200)
+
+}
+
 const Chat = (props) => {
   const [messages, setMessages] = useState(null)
+  const [variants, setVariants] = useState(null)
 
   useEffect(() => {
     async function getPrevMessages(){
@@ -23,7 +62,8 @@ const Chat = (props) => {
       const status = res.status
       console.log(status)
       const data = await res.json()
-      setMessages(data)
+      setMessages(data.messages)
+      setVariants(data.variants)
       console.log(data)
     }
     getPrevMessages()
@@ -50,6 +90,23 @@ const Chat = (props) => {
     }
   }
 
+  const variantsGeneratorr = () => {
+    if(variants){
+      return variants.map((item) =>{
+        if(item.variant[0] !== "#")
+        return <div className='variant' onClick={() => {sendChosenUpdate(
+          item.direction,
+          item.variant,
+          props.user,
+          setVariants,
+          setMessages,
+          document.getElementById('chatMain'),
+          messages
+        )}}>{item.variant}</div>
+      })
+    }
+  }
+
   return(
     <div className='chat-wrapper'>
       <div className='chat-main' id='chatMain'>
@@ -58,9 +115,14 @@ const Chat = (props) => {
       </div>
     <UserInput 
       userId = {props.user} 
-      setMessages = {setMessages} 
+      setMessages = {setMessages}
+      setVariants = {setVariants}
       chatMain ={document.getElementById('chatMain')}
-      messages = {messages}/>
+      messages = {messages}
+      variants = {variants}/>
+      <div className='variants-wrapper'>
+        {variantsGeneratorr()}
+      </div>
     </div>
   )
 }
--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Input } from 'antd'
+import React, { useState } from 'react';
+import { Input, Form } from 'antd'
 import { ArrowUpOutlined } from '@ant-design/icons'
 import './UserInput.css'
 
-async function sendUserUpdate(message, userId, setMessages , chatMain, messages){
+async function sendUserUpdate(message, userId, setMessages , chatMain, messages, variants, setVariants){
   setMessages([...messages, {sender: 'user', message: message}])
   setTimeout(() => {chatMain.scrollTop = chatMain.scrollHeight}, 40)
   //console.log(message, userId)
@@ -16,33 +16,46 @@ async function sendUserUpdate(message, userId, setMessages , chatMain, messages)
       },
       body: JSON.stringify({
         id:userId,
-        message:message
+        message:message,
+        variants: variants
       })
     })
   let status = result.status
   if(status === 200){
     let data = await result.json()
     console.log(data)
-    setMessages(data)
+    setMessages(data.messages)
+    setVariants(data.variants)
   }
   setTimeout(() => {chatMain.scrollTop = chatMain.scrollHeight}, 200)
 }
 
 const UserInput = (props) => {
+
+  const onFinish = () => {
+    setInputValue("")
+    sendUserUpdate(
+      inputValue,
+      props.userId,
+      props.setMessages,
+      props.chatMain,
+      props.messages,
+      props.variants,
+      props.setVariants)
+  }
+
+  const [inputValue, setInputValue] = useState("")
   return(
     <div className='user-input-block'>
-      <Input id="userInput" className='user-input' placeholder='Твое сообщение'/>
+      <Form style={{width: '100%'}} className='user-input' onFinish={onFinish}>
+      <Input value = {inputValue} onChange={(e) => setInputValue(e.target.value)} id="userInput" className='user-input' placeholder='Твое сообщение'/>
+      </Form>
         <button 
           className='user-input-button'
-          onClick={() => sendUserUpdate(
-            document.getElementById('userInput').value,
-            props.userId,
-            props.setMessages,
-            props.chatMain,
-            props.messages)
-            }>
+          onClick={onFinish}>
             <ArrowUpOutlined style={{color: 'white'}}/>
         </button>
+      
     </div>
   )
 }
